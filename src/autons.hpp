f82a9293dc82b3ba95d62f9e8ae80f0a5e1bf0b1
VY <amzingfood@duck.com>
2023-05-26 16:33:28 -0700

    a

--- src/autons.hpp
+++ src/autons.hpp
@@ -5,9 +5,18 @@ using namespace vex;
 //degrees to inches value for auton functions
 double dti = 23;
 
+//Forward function
 void For(double joe) {
     fl.spinFor(fwd,joe,deg,false);
     fr.spinFor(fwd,joe,deg,false);
     bl.spinFor(fwd,joe,deg,false);
     br.spinFor(fwd,joe,deg);
+}
+
+//Backward function
+void Rev(double joe) {
+    fl.spinFor(fwd,-joe,deg,false);
+    fr.spinFor(fwd,-joe,deg,false);
+    bl.spinFor(fwd,-joe,deg,false);
+    br.spinFor(fwd,-joe,deg);
 }
