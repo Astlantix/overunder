1215a790d5f6afc72f3862b255501339fffa9ed0
VY <amzingfood@duck.com>
2023-06-15 15:00:09 -0700

    asdlkfjadlskfjasd;lfja

---
+++ include/robot-config.h
@@ -0,0 +1,10 @@
+using namespace vex;
+
+extern brain Brain;
+
+
+// define your global instances of motors and other devices here
+extern motor fr;
+extern motor fl;
+extern motor br;
+extern motor bl;
--- src/main.cpp
+++ src/main.cpp
@@ -85,6 +85,10 @@ int main() {
   // Run the pre-autonomous function.
   pre_auton();
 
+
+
+
+
   // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);
