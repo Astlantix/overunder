d11213b1711c549c27487110c8f1430960b74efb
VY <amzingfood@duck.com>
2023-06-22 17:34:22 -0700

    a

--- src/autons.hpp
+++ src/autons.hpp
@@ -5,32 +5,32 @@ double dti = 23;
 
 //Forward function
 void For(double joe) {
-    fl.spinFor(fwd,joe,deg,false);
-    fr.spinFor(fwd,joe,deg,false);
-    bl.spinFor(fwd,joe,deg,false);
-    br.spinFor(fwd,joe,deg);
+    fl.spinFor(fwd,joe,degrees,false);
+    fr.spinFor(fwd,joe,degrees,false);
+    bl.spinFor(fwd,joe,degrees,false);
+    br.spinFor(fwd,joe,degrees);
 }
 
 //Backward function
 void Rev(double joe) {
-    fl.spinFor(rev,joe,deg,false);
-    fr.spinFor(rev,joe,deg,false);
-    bl.spinFor(rev,joe,deg,false);
-    br.spinFor(rev,joe,deg);
+    fl.spinFor(reverse,joe,degrees,false);
+    fr.spinFor(reverse,joe,degrees,false);
+    bl.spinFor(reverse,joe,degrees,false);
+    br.spinFor(reverse,joe,degrees);
 }
 
 //Left function
 void Left(double joe) {
-    fl.spinFor(rev,joe,deg,false);
-    fr.spinFor(fwd,joe,deg,false);
-    bl.spinFor(fwd,joe,deg,false);
-    br.spinFor(rev,joe,deg);
+    fl.spinFor(reverse,joe,degrees,false);
+    fr.spinFor(fwd,joe,degrees,false);
+    bl.spinFor(fwd,joe,degrees,false);
+    br.spinFor(reverse,joe,degrees);
 }
 
 //Right function
 void Right(double joe) {
-    fl.spinFor(fwd,joe,deg,false);
-    fr.spinFor(rev,joe,deg,false);
-    bl.spinFor(rev,joe,deg,false);
-    br.spinFor(fwd,joe,deg);
+    fl.spinFor(fwd,joe,degrees,false);
+    fr.spinFor(reverse,joe,degrees,false);
+    bl.spinFor(reverse,joe,degrees,false);
+    br.spinFor(fwd,joe,degrees);
 }
