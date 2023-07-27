81602dd809839f636202ba6170382625d0293424
VY <amzingfood@duck.com>
2023-07-27 01:15:42 -0700

    Update odometry.hpp

--- src/odometry.hpp
+++ src/odometry.hpp
@@ -3,10 +3,10 @@
 double π = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923542019956112129021960864034418159813629774771309960518707211349999998372978049951059731732816096318595024459455346908302642522308253344685035261931188171010003137838752886587533208381420617177669147303598253490428755468731159562863882353787593751957781857780532171226806613001927876611195909216420198;
 
 //replace 4 with your wheel diameter for odom pods (units don't matter as long as you use them all across the code)
-double wheelDiameter = 4;
-double wheelCircumference = wheelDiameter * π;
-double ticksPerRev = 360;
-double ticksPerInch = ticksPerRev / wheelCircumference;
+const double wheelDiameter = 2.75;
+const double wheelCircumference = wheelDiameter * π;
+const double ticksPerRev = 360;
+const double ticksPerInch = ticksPerRev / wheelCircumference;
 
 //forward and backward movement in inches (or whatever units you use)
 void fwdrev(double joe) {
@@ -29,6 +29,7 @@ void turn(double joe) {
 
 double x;
 double y;
+double heading;
 
 //odometry
 void odometry() {
@@ -38,15 +39,34 @@ void odometry() {
   while(1) {
     x = xpos.position(deg)*ticksPerInch;
     y = ypos.position(deg)*ticksPerInch;
+    heading = inert.rotation(deg);
     wait(20, msec);
   }
 }
+
+double quart;
 
 void travel(double xcor, double ycor) {
   double xdiff = xcor - x;
   double ydiff = ycor - y;
   double distance = sqrt(xdiff*xdiff + ydiff*ydiff);
-  double angleDiff = atan2(ydiff,xdiff) * 180 / π;
+  if(xdiff == 0) {
+    if(ydiff > 0) {
+      quart = 90;
+    } 
+    else {
+      quart = -90;
+    }
+  } 
+  if(ydiff == 0) {
+    if(xdiff > 0) {
+      quart = 0;
+    } 
+    else {
+      quart = 180;
+    }
+  }
+  double angleDiff = atan2(ydiff,xdiff) * 180 / π - heading - quart;
   turn(angleDiff);
   fwdrev(distance);
 }
