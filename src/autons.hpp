7b6a48eb1351c4b5346554b418024c36cfcea198
VY <amzingfood@duck.com>
2023-10-29 16:59:38 -0700

    update autons.hpp and main.cpp

--- src/autons.hpp
+++ src/autons.hpp
@@ -47,42 +47,42 @@ void stoop() {
 
 //Forward function
 void For(double joe) {
-    fl.spinFor(fwd,joe*36,degrees,false);
-    fr.spinFor(fwd,joe*36,degrees,false);
-    ml.spinFor(fwd,joe*36,degrees,false);
-    mr.spinFor(fwd,joe*36,degrees,false);
-    bl.spinFor(fwd,joe*36,degrees,false);
-    br.spinFor(fwd,joe*36,degrees);
+    fl.spinFor(fwd,joe*24,degrees,false);
+    fr.spinFor(fwd,joe*24,degrees,false);
+    ml.spinFor(fwd,joe*24,degrees,false);
+    mr.spinFor(fwd,joe*24,degrees,false);
+    bl.spinFor(fwd,joe*24,degrees,false);
+    br.spinFor(fwd,joe*24,degrees);
 }
 
 //Backward function
 void Rev(double joe) {
-    fl.spinFor(reverse,joe*36,degrees,false);
-    fr.spinFor(reverse,joe*36,degrees,false);
-    ml.spinFor(reverse,joe*36,degrees,false);
-    mr.spinFor(reverse,joe*36,degrees,false);
-    bl.spinFor(reverse,joe*36,degrees,false);
-    br.spinFor(reverse,joe*36,degrees);
+    fl.spinFor(reverse,joe*24,degrees,false);
+    fr.spinFor(reverse,joe*24,degrees,false);
+    ml.spinFor(reverse,joe*24,degrees,false);
+    mr.spinFor(reverse,joe*24,degrees,false);
+    bl.spinFor(reverse,joe*24,degrees,false);
+    br.spinFor(reverse,joe*24,degrees);
 }
 
 //Left function
 void Left(double joe) {
-    fl.spinFor(reverse,joe*3.3515,degrees,false);
-    fr.spinFor(fwd,joe*3.3515,degrees,false);
-    ml.spinFor(reverse,joe*3.3515,degrees,false);
-    mr.spinFor(fwd,joe*3.3515,degrees,false);
-    bl.spinFor(reverse,joe*3.3515,degrees,false);
-    br.spinFor(reverse,joe*3.3515,degrees);
+    fl.spinFor(reverse,joe*2,degrees,false);
+    fr.spinFor(fwd,joe*2,degrees,false);
+    ml.spinFor(reverse,joe*2,degrees,false);
+    mr.spinFor(fwd,joe*2,degrees,false);
+    bl.spinFor(reverse,joe*2,degrees,false);
+    br.spinFor(reverse,joe*2,degrees);
 }
 
 //Right function
 void Right(double joe) {
-    fl.spinFor(fwd,joe*3.3515,degrees,false);
-    fr.spinFor(reverse,joe*3.3515,degrees,false);
-    ml.spinFor(fwd,joe*3.3515,degrees,false);
-    mr.spinFor(reverse,joe*3.3515,degrees,false);
-    bl.spinFor(fwd,joe*3.3515,degrees,false);
-    br.spinFor(reverse,joe*3.3515,degrees);
+    fl.spinFor(fwd,joe*2,degrees,false);
+    fr.spinFor(reverse,joe*2,degrees,false);
+    ml.spinFor(fwd,joe*2,degrees,false);
+    mr.spinFor(reverse,joe*2,degrees,false);
+    bl.spinFor(fwd,joe*2,degrees,false);
+    br.spinFor(reverse,joe*2,degrees);
 }
 
 void setv(double joe) {
@@ -99,19 +99,16 @@ void setv(double joe) {
 void auton1 () {
 
     setbrake();
-    setv(70);
+    setv(50);
     fly.close();
+    wait(20,msec);
     Rev(15);
+    wait(20,msec);
     Right(60);
+    wait(20,msec);
     fly.open();
+    wait(20,msec);
     Rev(22);
-    For(15);
-    Left(55);
-    For(15);
-    Right(90);
-    For(24);
-    Left(90);
-    For(24);
     setcoast();
     c = 1;
 }
@@ -177,6 +174,8 @@ void auton3 () {
     setbrake();
 
     Right(90);
+    wait(200,msec);
+    Rev(24);
 
     setcoast();
     c = 1;
