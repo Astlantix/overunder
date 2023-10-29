13a23de10ede4f3e834532cc3bb2e10d680d4e65
VY <amzingfood@duck.com>
2023-10-29 09:36:36 -0700

    update 5 files

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -8,12 +8,12 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor fl = motor(PORT17, ratio18_1, false);
-motor fr = motor(PORT12, ratio18_1, true);
-motor bl = motor(PORT4, ratio18_1, false);
-motor br = motor(PORT1, ratio18_1, true);
-motor ml = motor(PORT10, ratio18_1, false);
-motor mr = motor(PORT7, ratio18_1, true);
+motor fl = motor(PORT10, ratio18_1, true);
+motor fr = motor(PORT1, ratio18_1, false);
+motor bl = motor(PORT7, ratio18_1, true);
+motor br = motor(PORT2, ratio18_1, false);
+motor ml = motor(PORT5, ratio18_1, false);
+motor mr = motor(PORT4, ratio18_1, true);
 controller gamers = controller(primary);
 motor intake = motor(PORT16, ratio18_1, false);
 motor pull = motor(PORT11, ratio36_1, false);
@@ -23,7 +23,7 @@ rotation rig = rotation(PORT2, false);
 rotation side = rotation(PORT3, false);
 pneumatics wings = pneumatics(Brain.ThreeWirePort.A);
 pneumatics fly = pneumatics(Brain.ThreeWirePort.C);
-motor cata = motor(PORT5, ratio36_1, false);
+motor cata = motor(PORT17, ratio36_1, false);
 limit ballin = limit(Brain.ThreeWirePort.E);
 
 // VEXcode generated functions
