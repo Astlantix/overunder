030d82d0f9b94b1b0690b19f2e44b1f5be51243c
VY <amzingfood@duck.com>
2023-11-17 08:43:04 -0800

    INERTIAL IS SCARY I HATE INERTIAL HELP

---
+++ src/pid.cpp
@@ -0,0 +1,97 @@
+#include "vex.h"
+
+using namespace vex;
+
+
+// PID constants
+double kP = 0.0;
+double kI = 0.0;
+double kD = 0.0;
+
+// PID variables
+double integral = 0;
+double derivative = 0;
+double error = 0;
+double lastError = 0;
+double integralLimit = 20;
+
+// Orientation PID constants
+double kP_orient = 0.0;
+double kI_orient = 0.0;
+double kD_orient = 0.0;
+
+// Orientation PID variables
+double integral_orient = 0;
+double derivative_orient = 0;
+double error_orient = 0;
+double lastError_orient = 0;
+
+void drivetrainPID(double targetDistance, double targetOrientation, double distanceSensorValue, double orientationSensorValue) {
+    // Calculate distance error
+    error = targetDistance - distanceSensorValue;
+
+    // Calculate orientation error
+    error_orient = targetOrientation - orientationSensorValue;
+
+    // Calculate distance integral with windup prevention
+    if (fabs(error) < integralLimit) {
+        integral += error;
+    } else {
+        integral = 0;
+    }
+
+    // Calculate orientation integral with windup prevention
+    if (fabs(error_orient) < integralLimit) {
+        integral_orient += error_orient;
+    } else {
+        integral_orient = 0;
+    }
+
+    // Calculate distance derivative
+    derivative = error - lastError;
+    lastError = error;
+
+    // Calculate orientation derivative
+    derivative_orient = error_orient - lastError_orient;
+    lastError_orient = error_orient;
+
+    // Calculate distance PID output
+    double output = kP * error + kI * integral + kD * derivative;
+
+    // Calculate orientation PID output
+    double output_orient = kP_orient * error_orient + kI_orient * integral_orient + kD_orient * derivative_orient;
+
+    // Control motors
+    double leftMotorSpeed = output - output_orient;
+    double rightMotorSpeed = output + output_orient;
+
+    // Set motor speeds...
+    L.spin(fwd, leftMotorSpeed, pct);
+    R.spin(fwd, rightMotorSpeed, pct);
+}
+
+void travel(double distance, double angle) {
+    // Get current distance sensor value
+    double currentDistance = D.position(turns) * 4;
+
+    // Get current orientation sensor value
+    double currentOrientation = inert.yaw();
+
+    // Move forward
+    while (fabs(distance - currentDistance) > 0.1) {
+        drivetrainPID(distance, currentOrientation, currentDistance, currentOrientation);
+        currentDistance = D.position(turns) *4;
+    }
+
+    // Turn
+    while (fabs(angle - currentOrientation) > 0.1) {
+        drivetrainPID(0, angle, currentDistance, currentOrientation);
+        currentOrientation = inert.yaw();
+    }
+
+    // Move forward again
+    while (fabs(distance - currentDistance) > 0.1) {
+        drivetrainPID(distance, currentOrientation, currentDistance, currentOrientation);
+        currentDistance = D.position(turns) *4;
+    }
+}
